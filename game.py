7c884959a0e01d88e457093c04e78034d1925440
Madeleine Nielsen <168845829+Ms-Nielsen@users.noreply.github.com>
2025-06-02 14:49:15 -0400

    Update game.py

--- game.py
+++ game.py
@@ -13,24 +13,25 @@ st.write("In this game, two players will compete to guess a number in the fastes
 time.sleep(3)
 
 def name_entries(num):
-  player1 = st.text_input("Enter name of player " + num + ": ")
-  return player1
+  player = st.text_input("Enter name of player " + num + ": ")
+  return player
   
-def getGuess(name1, name2):
+def getGuess(name):
   st.write(name + " it is your turn.")
-  guess = st.slider("Guess a number between 1 and 10", min_value = 1, max_value=10, on_change=getGuess(name2))
+  guess = st.slider("Guess a number between 1 and 10", min_value = 1, max_value=10)
   return guess
   
 def main():
 
   player1name = name_entries('1')
+  
   if player1name!='':
     player2name=name_entries('2')
     
-  p1guess=""
-  
-  if player1name !='' and player2name!="":
-    p1guess = getGuess(player1name, player2name)
+  p1guess = getGuess(player1name)
+
+  if st.button("Continue"):
+    p2guess = getGuess(player2name)
     
 
 main()
