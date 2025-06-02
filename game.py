dc687aef45c649f372fee5faf8d90eaf2fbf9470
Madeleine Nielsen <168845829+Ms-Nielsen@users.noreply.github.com>
2025-06-02 10:10:15 -0400

    Update game.py

--- game.py
+++ game.py
@@ -1,10 +1,9 @@
 import streamlit as st
 
-st.write
-print('Hello world, today you two players will be playing a guessing game to guess a number between 1 and 10. You will each have three tries.')
+st.write('Hello world, today you two players will be playing a guessing game to guess a number between 1 and 10. You will each have three tries.')
 
-player1 = input("Enter name of player 1: ")
-player2 = input("Enter name of player 2: ")
+player1 = st.text_input("Enter name of player 1: ")
+player2 = st.text_input("Enter name of player 2: ")
 p1wins = 0
 p2wins = 0
 gameround = 1
