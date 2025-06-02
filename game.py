ad55ff8e28d5e5e81d24144e99fbbdb6d740bba9
Madeleine Nielsen <168845829+Ms-Nielsen@users.noreply.github.com>
2025-06-02 10:21:26 -0400

    Update game.py

--- game.py
+++ game.py
@@ -1,10 +1,4 @@
 import streamlit as st
 
-st.write("Hello world")
 
-player1 = st.text_input("Enter name of player 1: ")
-player2 = st.text_input("Enter name of player 2: ")
-p1wins = 0
-p2wins = 0
-gameround = 1
 
